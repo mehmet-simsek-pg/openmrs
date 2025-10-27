f5181f724606cec2fb2d34d7f0950a30bbbaa5fa
Mehmet Simsek <simsekmehmet00@gmail.com>
2025-10-27 17:23:03 +0100

    BasePage and BaseDriver created

---
+++ src/test/java/utility/BaseDriver.java
@@ -0,0 +1,35 @@
+package utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.time.Duration;
+
+public class BaseDriver {
+
+    public static WebDriver driver;
+
+    public static WebDriver driver(String url, String browser) {
+
+        switch (browser) {
+            case "chrome": driver = new ChromeDriver();break;
+            case "firefox": driver = new FirefoxDriver();break;
+            case "safari": driver = new SafariDriver();break;
+        }
+        driver.get(url);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        return driver;
+    }
+
+    public static void threadWait(int second) {
+        try {
+            Thread.sleep(second * 1000L);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
